04933618ebc356dfe40da8b0a3fa71a9b092a10b
https://gitlab.droidox.com/hovhannes <hovhannes@tigra.app>
2023-01-10 16:27:50 +0400

    MEVN stack

---
+++ client/src/components/Application/NavMenu.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-navigation-drawer
+    app
+    clipped
+    color="success"
+    v-model="value"
+    :input="value"
+    @update:mini-variant="$emit('input', value)"
+  >
+    <v-list nav>
+      <v-list-item
+        class="bright--text"
+        v-for="navLink in navLinks"
+        :key="navLink.route"
+        :to="navLink.route"
+        link
+      >
+        <v-list-item-icon>
+          <v-icon color="bright">{{ navLink.icon }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          {{ navLink.text }}
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'NavMenu',
+
+  props: {
+    value: {
+      type: Boolean,
+      required: true,
+    },
+  },
+
+  data: () => ({
+    navLinks: [ 
+      {
+        text: 'Users',
+        route: '/',
+        icon: 'mdi-account-group',
+      },
+    ],
+  }),
+}
+</script>
